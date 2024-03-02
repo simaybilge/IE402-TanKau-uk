ae7c50c938a3eb6427471002da663e6365bdb752
cerensutan <155387932+cerensutan@users.noreply.github.com>
2024-03-02 15:24:02 +0300

    Add files via upload

--- GifflerThompson.java
+++ GifflerThompson.java
@@ -1,31 +1,21 @@
-import java.sql.SQLOutput;
+import java.lang.reflect.Array;
 import java.util.*;
 
 public class GifflerThompson {
-    //public static final int[] daysOfWeek = {0, 1, 2, 3, 4}; // Example: Operating on weekdays
-    //public static final int NUMBER_OF_WORKERS = 5;
-    //public static final double OPERATING_HOURS_PER_DAY = 8 * 60; // 8 hours in minutes
+    static ArrayList<Truck> trucks = Truck.generateTrucksFromCSV("tan_kaucuk_data.csv");
+    static ArrayList<Truck> orderedList = new ArrayList<Truck>();
+    static ArrayList<Truck> dockedOrderedList = new ArrayList<>();
+    static ArrayList<Truck> notDockedOrderedList = new ArrayList<>();
+    static HashMap<Truck, Double> criticalRatioList = new HashMap<>();
+    static boolean dockedAvailable;
+    static boolean notDockedAvailable;
 
-    /*Step 1: Bütün truckları okuycak listeleri doldurucak (DONE)
-    Step 2: Bütün trucklar için critical ratio hesaplanacak ve bir listede storelanacak (DONE)
-    Step 3: Critical ratio listesi sıralanacak ((due time-arrival time)/process time) (DONE)
-    Step 4: Priority'si büyük olanı önce alacak şekilde sıralanacak (DONE)
-    Step 4: Worker, due time, foreign/domestic constraintlerine göre sıralar hesaplacak
-
-        1) foreign ise sadece perşembe/cuma işlenebilir
-        2) foreign kamyonların priority'si daha yüksek olacak
-        3) aynı anda docked ve not docked çalışabilir, iki tane aynı tip çalışamaz
-        4) aynı anda çalışan kamyonların toplam worker sayısı en fazla 5 olabilir
-        5) critical ratio negatif olursa sıranın en sonuna at (sadece bir taneden penalty yemek için) (DONE)
-
-    */
     public static void main(String[] args) {
+        calculateQueues();
+        processWeek(dockedOrderedList,notDockedOrderedList,dockedAvailable,notDockedAvailable);
+    }
 
-        ArrayList<Truck> trucks = Truck.generateTrucksFromCSV("tan_kaucuk_data.csv");
-        ArrayList<Truck> orderedList = new ArrayList<Truck>();
-        ArrayList<Truck> dockedOrderedList = new ArrayList<>();
-        ArrayList<Truck> notDockedOrderedList = new ArrayList<>();
-        HashMap<Truck, Double> criticalRatioList = new HashMap<>();
+    public static void calculateQueues() {
         for (int i = 0; i < trucks.size(); i++) {
             criticalRatioList.put(trucks.get(i), trucks.get(i).getCriticalRatio());
         }
@@ -38,18 +28,18 @@ public class GifflerThompson {
         }
 
         for (int i = 0; i < 19; i++) { //0'dan 18'e
-            if(orderedList.get(i).getCriticalRatio() == orderedList.get(i+1).getCriticalRatio()){
-                if(orderedList.get(i).getPriority() < orderedList.get(i+1).getPriority()){
+            if (orderedList.get(i).getCriticalRatio() == orderedList.get(i + 1).getCriticalRatio()) {
+                if (orderedList.get(i).getPriority() < orderedList.get(i + 1).getPriority()) {
                     Truck temp = null;
                     temp = orderedList.get(i);
-                    orderedList.set(i,orderedList.get(i+1));
-                    orderedList.set(i+1,temp);
+                    orderedList.set(i, orderedList.get(i + 1));
+                    orderedList.set(i + 1, temp);
                 }
             }
         }
 
-        for(Truck t: orderedList){
-            if(t.getIsDocked()){
+        for (Truck t : orderedList) {
+            if (t.getIsDocked()) {
                 dockedOrderedList.add(t);
             } else {
                 notDockedOrderedList.add(t);
@@ -59,31 +49,86 @@ public class GifflerThompson {
         System.out.println("List of docked trucks ordered according to critical ratio and priority: ");
         System.out.println(" ");
 
-        for(int i = 0; i < dockedOrderedList.size(); i++) {
-            System.out.println("Order: " + (i+1) + " Truck ID: " + dockedOrderedList.get(i).getId() + " Critical ratio: " + String.format("%.2f",dockedOrderedList.get(i).getCriticalRatio()) + " Priority: " + dockedOrderedList.get(i).getPriority() + " Is it a foreign truck?:  " + dockedOrderedList.get(i).getIsForeign());
+        for (int i = 0; i < dockedOrderedList.size(); i++) {
+            System.out.println("Order: " + (i + 1));
+            System.out.println("Truck ID: " + dockedOrderedList.get(i).getId());
+            System.out.println("Critical ratio: " + String.format("%.2f", dockedOrderedList.get(i).getCriticalRatio()));
+            System.out.println("Priority: " + dockedOrderedList.get(i).getPriority());
+            System.out.println("Number of workers needed: " + dockedOrderedList.get(i).getNeededWorkers());
+            System.out.println("Is it a foreign truck? : " + dockedOrderedList.get(i).getIsForeign());
+            System.out.println("---------------------------------------");
         }
 
         System.out.println(" ");
         System.out.println("List of not docked trucks ordered according to critical ratio and priority: ");
         System.out.println(" ");
 
-        for(int i = 0; i < notDockedOrderedList.size(); i++) {
-            System.out.println("Order: " + (i+1) + " Truck ID: " + notDockedOrderedList.get(i).getId() + " Critical ratio: " + String.format("%.2f",notDockedOrderedList.get(i).getCriticalRatio()) + " Priority: " + notDockedOrderedList.get(i).getPriority() + " Is it a foreign truck?:  " + notDockedOrderedList.get(i).getIsForeign());
+        for (int i = 0; i < notDockedOrderedList.size(); i++) {
+            System.out.println("Order: " + (i + 1));
+            System.out.println("Truck ID: " + notDockedOrderedList.get(i).getId());
+            System.out.println("Critical ratio: " + String.format("%.2f", notDockedOrderedList.get(i).getCriticalRatio()));
+            System.out.println("Priority: " + notDockedOrderedList.get(i).getPriority());
+            System.out.println("Number of workers needed: " + notDockedOrderedList.get(i).getNeededWorkers());
+            System.out.println("Is it a foreign truck? : " + notDockedOrderedList.get(i).getIsForeign());
+            System.out.println("---------------------------------------");
         }
 
     }
 
-
-    private static Truck findNextBestRatioAndRemove(HashMap<Truck,Double> criticalRatioList) {
+    private static Truck findNextBestRatioAndRemove(HashMap<Truck, Double> criticalRatioList) {
         Truck best = null;
         double bestRatio = -Integer.MAX_VALUE;
-        for (Truck s: criticalRatioList.keySet()){
-            if(criticalRatioList.get(s)>bestRatio){
-                bestRatio=criticalRatioList.get(s);
-                best=s;
+        for (Truck s : criticalRatioList.keySet()) {
+            if (criticalRatioList.get(s) > bestRatio) {
+                bestRatio = criticalRatioList.get(s);
+                best = s;
             }
         }
         criticalRatioList.remove(best);
         return best;
     }
+
+    public static Truck chooseNextTruckAndRemove(ArrayList<Truck> orderedList, int availableWorkers) {
+        Truck nextTruck = null;
+        for (Truck t : orderedList) {
+            if (availableWorkers >= t.getNeededWorkers()) {
+                nextTruck = t;
+            } else {
+                continue;
+            }
+        }
+        orderedList.remove(nextTruck);
+        return nextTruck;
+    }
+
+    public static void processWeek(ArrayList<Truck> dockedOrderedList, ArrayList<Truck> notDockedOrderedList, boolean dockedAvailable, boolean notDockedAvailable) {
+        Truck nextDockedTruck = null;
+        Truck nextNotDockedTruck = null;
+        dockedAvailable = true;
+        notDockedAvailable = true;
+        int availableWorkers = 5;
+        int dockedStartTime = 0;
+        int notDockedStartTime = 0;
+
+        for (int day = 0; day <= 4; day++) {
+            for (int minute = 0; minute <= 480; minute++) {
+                if (dockedAvailable) {
+                    nextDockedTruck = chooseNextTruckAndRemove(dockedOrderedList, availableWorkers);
+                    processTruck(nextDockedTruck, dockedAvailable);
+                    dockedStartTime = day * 480 + minute;
+                } else if (notDockedAvailable) {
+                    nextNotDockedTruck = chooseNextTruckAndRemove(notDockedOrderedList, availableWorkers);
+                    processTruck(nextNotDockedTruck, notDockedAvailable);
+                    notDockedStartTime = day * 480 + minute;
+                } else {
+                    continue;
+                }
+            }
+        }
+    }
+
+    public static void processTruck(Truck nextTruck, boolean available){
+
+    }
 }
+
