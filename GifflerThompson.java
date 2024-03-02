5a337cb3cb5bf14e80e8146da72ddf5792664a44
cerensutan <155387932+cerensutan@users.noreply.github.com>
2024-03-02 11:49:52 +0300

    Add files via upload

--- GifflerThompson.java
+++ GifflerThompson.java
@@ -1,3 +1,4 @@
+import java.sql.SQLOutput;
 import java.util.*;
 
 public class GifflerThompson {
@@ -8,18 +9,22 @@ public class GifflerThompson {
     /*Step 1: Bütün truckları okuycak listeleri doldurucak (DONE)
     Step 2: Bütün trucklar için critical ratio hesaplanacak ve bir listede storelanacak (DONE)
     Step 3: Critical ratio listesi sıralanacak ((due time-arrival time)/process time) (DONE)
-    Step 4: Worker, due time, foreign/domestic constraintlerine göre sıralar hesaplacak (setup cost??)
+    Step 4: Priority'si büyük olanı önce alacak şekilde sıralanacak (DONE)
+    Step 4: Worker, due time, foreign/domestic constraintlerine göre sıralar hesaplacak
 
         1) foreign ise sadece perşembe/cuma işlenebilir
-        2) aynı anda docked ve not docked çalışabilir, iki tane aynı tip çalışamaz
-        3) aynı anda çalışan kamyonların toplam worker sayısı en fazla 5 olabilir
-        4) critical ratio negatif olursa sıranın en sonuna at (sadece bir taneden penalty yemek için) (DONE)
-        5) priority'ye göre sıralasın
+        2) foreign kamyonların priority'si daha yüksek olacak
+        3) aynı anda docked ve not docked çalışabilir, iki tane aynı tip çalışamaz
+        4) aynı anda çalışan kamyonların toplam worker sayısı en fazla 5 olabilir
+        5) critical ratio negatif olursa sıranın en sonuna at (sadece bir taneden penalty yemek için) (DONE)
+
     */
     public static void main(String[] args) {
 
         ArrayList<Truck> trucks = Truck.generateTrucksFromCSV("tan_kaucuk_data.csv");
         ArrayList<Truck> orderedList = new ArrayList<Truck>();
+        ArrayList<Truck> dockedOrderedList = new ArrayList<>();
+        ArrayList<Truck> notDockedOrderedList = new ArrayList<>();
         HashMap<Truck, Double> criticalRatioList = new HashMap<>();
         for (int i = 0; i < trucks.size(); i++) {
             criticalRatioList.put(trucks.get(i), trucks.get(i).getCriticalRatio());
@@ -41,10 +46,34 @@ public class GifflerThompson {
                     orderedList.set(i+1,temp);
                 }
             }
-            System.out.println("order: " + (i + 1) + " truck id: " + orderedList.get(i).getId() + " critical ratio: " + String.format("%.2f",orderedList.get(i).getCriticalRatio()));
         }
-        System.out.println("order: " + (20) + " truck id: " + orderedList.get(19).getId() + " critical ratio: " + String.format("%.2f",orderedList.get(19).getCriticalRatio()));
+
+        for(Truck t: orderedList){
+            if(t.getIsDocked()){
+                dockedOrderedList.add(t);
+            } else {
+                notDockedOrderedList.add(t);
+            }
+        }
+
+        System.out.println("List of docked trucks ordered according to critical ratio and priority: ");
+        System.out.println(" ");
+
+        for(int i = 0; i < dockedOrderedList.size(); i++) {
+            System.out.println("Order: " + (i+1) + " Truck ID: " + dockedOrderedList.get(i).getId() + " Critical ratio: " + String.format("%.2f",dockedOrderedList.get(i).getCriticalRatio()) + " Priority: " + dockedOrderedList.get(i).getPriority() + " Is it a foreign truck?:  " + dockedOrderedList.get(i).getIsForeign());
+        }
+
+        System.out.println(" ");
+        System.out.println("List of not docked trucks ordered according to critical ratio and priority: ");
+        System.out.println(" ");
+
+        for(int i = 0; i < notDockedOrderedList.size(); i++) {
+            System.out.println("Order: " + (i+1) + " Truck ID: " + notDockedOrderedList.get(i).getId() + " Critical ratio: " + String.format("%.2f",notDockedOrderedList.get(i).getCriticalRatio()) + " Priority: " + notDockedOrderedList.get(i).getPriority() + " Is it a foreign truck?:  " + notDockedOrderedList.get(i).getIsForeign());
+        }
+
     }
+
+
     private static Truck findNextBestRatioAndRemove(HashMap<Truck,Double> criticalRatioList) {
         Truck best = null;
         double bestRatio = -Integer.MAX_VALUE;
